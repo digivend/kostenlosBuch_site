5c9befb9e5f9a85578018d02c80d1db6e758f25c
checker21 <checker21@users.noreply.github.com>
2020-03-20 06:08:42 +0100

    add About App

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 	import ProductList from './components/ProductList.svelte';
 	import Impressum from './components/Impressum.svelte';
 	import Datenschutz from './components/Datenschutz.svelte';
+	import About from './components/About.svelte';
 	import Footer from './components/Footer.svelte';
 
 	import queryString from "query-string";
@@ -20,6 +21,8 @@
 
 	{#if pId == "impressum"}
 		<Impressum />
+	{:else if pId == "about"}
+		<About />
 	{:else if pId == "datenschutz"}
 		<Datenschutz />
 	{:else if pId == "abmelden"}
