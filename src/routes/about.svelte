a0b491796ef249c18123c04372ca66ada4d21549
checker21 <checker21@users.noreply.github.com>
2020-04-09 11:54:57 +0200

    rechtschreibfehler behoben

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,7 +3,7 @@
   <h2>Jeden Tag ein Buch lesen</h2>
   <p>Wir leben heute in einer Welt die sich so schnell wie noch nie weiterentwickelt. Damit du mit dem Wandel Schritt halten kannst, ist Weiterbildung wichtiger denn je geworden. Eine gute Methode ist regelmäßig ein Buch zu lesen, beispielsweise ein Buch pro Monat. Aber geht das nicht ins Geld? Nicht unbedingt.</p>
   <h2>Wir stellen euch kostenlose Bücher vor</h2>
-  <p>Mitterweile gibt es viele Autoren, die ein kostenloses Buch veröffentlichen. Allerdings sind diese schwer im Internet zu finden und gelten nur für einen bestimmten Zeitraum. Unser Mission ist es euch für die Lebensfelder: Finanzen, Persönlichkeit, Gesundheit, Beruf und Familie kostenlose Bücher vorstellen. Auf dass ihr Erfüllung in allen euren Lebensfelder findet.</p>
+  <p>Mittlerweile gibt es viele Autoren, die ein kostenloses Buch veröffentlichen. Allerdings sind diese schwer im Internet zu finden und gelten nur für einen bestimmten Zeitraum. Unser Mission ist es euch für die Lebensfelder: Finanzen, Persönlichkeit, Gesundheit, Beruf und Familie kostenlose Bücher vorstellen. Auf dass ihr Erfüllung in allen euren Lebensfelder findet.</p>
   <h2>Wie bekomme ich ein Buch kostenlos?</h2>
   <p>Schaut immer wieder auf unserer Seite vorbei oder abonniert unsern Newsletter um nicht die neusten gratis bzw. kostenlose Bücher Deals zu verpassen. Wir stellen euch regelmäßig kostenlose Bücher vor, für die Ihr nur eine Handlings Pauschale zahlen müsst. Damit ihr diese schnell und einfach bestellen könnt, setzen wir einen (*=Partnerlink) direkt zum Webauftritt des Autors.</p>
   <p>Viel Spaß beim Geldsparen!<br/>
