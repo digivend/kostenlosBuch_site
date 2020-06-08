db617a5755ac828b4fccdcaf785c24b0ba2ca9f5
maddeye <maddeye@DESKTOP-S97IPSG.localdomain>
2020-06-08 14:06:04 +0200

    remove load more function

--- src/components/Productlist.svelte
+++ src/components/Productlist.svelte
@@ -4,10 +4,7 @@
 
 	let products = [];
 
-	let itemPerLoad = 5;
-
-	$: loaded = itemPerLoad
-	$: loadedItems = products.slice(0, loaded);
+	$: loadedItems = products;
 
 	onMount(async()=>{
 		const response = await fetch("/content.json")
@@ -15,10 +12,6 @@
 		products = resp;
 		products.reverse();
 	});
-
-	function loadmore() {
-		loaded += itemPerLoad;
-	}
 
 </script>
 
@@ -34,10 +27,6 @@
     {#each loadedItems as product}
         <Product {product} />
     {/each}
-
-    {#if loaded < products.length}
-        <button class="laodmore" on:click={loadmore}>Mehr laden</button>
-    {/if}
 
 	<script id="n2g_script">!function(e,t,n,c,r,a,i){e.Newsletter2GoTrackingObject=r,e[r]=e[r]||function(){(e[r].q=e[r].q||[]).push(arguments)},e[r].l=1*new Date,a=t.createElement(n),i=t.getElementsByTagName(n)[0],a.async=1,a.src=c,i.parentNode.insertBefore(a,i)}(window,document,"script","https://static.newsletter2go.com/utils.js","n2g");var config = {"container": {"type": "div","class": "","style": ""},"row": {"type": "div","class": "","style": "margin-top: 15px;"},"columnLeft": {"type": "div","class": "","style": ""},"columnRight": {"type": "div","class": "","style": ""},"label": {"type": "label","class": "","style": ""}};n2g('create', 'm4baufab-tbrsch9z-24q');n2g('subscribe:createPopup', config, 10);</script>
 </div>
