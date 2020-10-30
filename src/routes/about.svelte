98e309cf848021d1ed62be1c0b2dd9ef58cce589
Robert Soellner <robert.soellner@siemens-healthineers.com>
2020-10-30 12:33:00 +0100

    add more questions and changed some style

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,10 +3,25 @@
   <h2>Jeden Tag ein Buch lesen</h2>
   <p>Wir leben heute in einer Welt die sich so schnell wie noch nie weiterentwickelt. Damit du mit dem Wandel Schritt halten kannst, ist Weiterbildung wichtiger denn je geworden. Eine gute Methode ist regelmäßig ein Buch zu lesen, beispielsweise ein Buch pro Monat. Aber geht das nicht ins Geld? Nicht unbedingt.</p>
   <h2>Wir stellen euch kostenlose Bücher vor</h2>
-  <p>Mittlerweile gibt es viele Autoren, die ein kostenloses Buch veröffentlichen. Allerdings sind diese schwer im Internet zu finden und gelten nur für einen bestimmten Zeitraum. Unser Mission ist es euch für die Lebensfelder: Finanzen, Persönlichkeit, Gesundheit, Beruf und Familie kostenlose Bücher vorstellen. Auf dass ihr Erfüllung in allen euren Lebensfelder findet.</p>
+  <p>Mittlerweile gibt es viele Autoren, die ein kostenloses Buch veröffentlichen. Allerdings sind diese schwer im Internet zu finden und gelten nur für einen bestimmten Zeitraum. Unser Mission ist es euch zu Helfen ein selbstbestimmtes Leben zu führen. Dies bedeutet, dass ihr eure Persönlichkeit entwickelt müsst um zur Besten Version von euch selbst zu werden. Um das Ziel zu erreichen, solltet ihr euch in allen Lebensfeldern weiterbilden. Daher zeigen wir euch die kostenlose Bücher aus den Lebensfelder: Finanzen, Persönlichkeit, Gesundheit, Beruf und Familie - Bücher die euch helfen.  Auf dass ihr Erfüllung in allen euren Lebensfelder findet und zu der Person werdet, die ihr immer schon sein wolltet.</p>
+  <h2>Sind die Bücher wirklich kostenlos?</h2>
+  <p>Ja, die Bücher an sich sind kostenlos. Du musst lediglich eine Handlingspauschale für Versand und Logistik bezahlen.</p>
+  <h2>Welche Autoren gibt es?</h2>
+  <p>Es gibt viele namenhafte Autoren die über Persönlichkeitsentwicklung schreiben und ihre Bücher verschenken. Dazu zählen u.a. Spiegel Besteller Autoren wie Bodo Schäfer, Hermann Scherer oder Prof. Dr. Jörg Knoblauch um nur einige zu nennen. Auf unsere <a href="kostenlos-buch.de">Webseite</a> findet Ihr inzwischen mehr als 60 Bücher zu diesen Themen. Viel Spaß beim Schmökern.</p>
+  <h2>Ist das Angebot seriös? Sind die Autoren Betrüger?</h2>
+  <p>Die Qualität der kostenlosen Bücher zum Thema selbstbestimmt Leben bzw. Entwicklung deiner Persönlichkeit schwankt von Autor zu Autor. Du bekommst auf jeden Fall immer das bestellte Buch geliefert. Inwieweit das Buch für Dich hilfreich ist, hängt zu einem von deiner Erwartungshaltung sowie von deinem Vorwissen ab. Wie bei jeden Buch kann es passieren, das der Schreibstil des Autors Dir nicht liegt. Ich habe schon einige von den Büchern gelesen und konnte bis jetzt immer etwas für mich mitnehmen und umsetzen.</p>
+  <h2>Warum verschenken die Autoren die Bücher?</h2>
+  <p>Die Gründe warum die Autoren von Bücher zum selbstbestimmt leben bzw. Persönlichkeitsentwicklung verschenken sind vielschichtig. Einige tun es bestimmt aus Karma Gründen, um ihr Wissen und Ruhm mit anderen zu teilen um die Welt zu verbessern. Für den größten Teil, aber wird es wirtschaftliche Beweggründe geben. Sie verkaufen ihre Bücher kostenlos um ihre Reichweite zu steigern, ihren Experten Status auf ihrem Gebiet zu manifestieren, Menschen von sich zu überzeugen und über Upselling oder folge Webinare bzw. Seminare Geld zu verdienen.</p>
+  <h2>Gehe ich ein Abo ein?</h2>
+  <p>Nein, es handelt sich nicht um einen Buchclub oder ähnliches. Auf den Verkaufsseiten des Autors wird transparent angezeigt, welche Produkte oder Dienstleistungen Ihr bestellt. </p>
+  <h2>Wann kann ich mit dem Buch rechnen?</h2>
+  <p>Meistens nutzen die Autoren der kostenlosen Bücher einen Print On Demand Dienstleister. Daher betragen die Lieferzeiten meisten zwischen 2-3 Tagen. In Einzelfällen kann es auch schon mal bis zu 14 Tagen dauern. In der aktuellen Situation sind die Lieferzeiten schwer einzuschätzen.</p>
   <h2>Wie bekomme ich ein Buch kostenlos?</h2>
   <p>Schaut immer wieder auf unserer Seite vorbei oder abonniert unsern Newsletter um nicht die neusten gratis bzw. kostenlose Bücher Deals zu verpassen. Wir stellen euch regelmäßig kostenlose Bücher vor, für die Ihr nur eine Handlings Pauschale zahlen müsst. Damit ihr diese schnell und einfach bestellen könnt, setzen wir einen (*=Partnerlink) direkt zum Webauftritt des Autors.</p>
-  <p>Viel Spaß beim Geldsparen!<br/>
-  Euer kostenlos Buch Team</p> 
-    
+  <p>Viel Spaß beim Geldsparen!<br/><br/>Euer kostenlos Buch Team</p>
+  <p>
+  <a href="https://kostenlos-buch.de/"><button class="featurebutton"><i class="fas fa-book"></i> Hier findest du hilfreiche Bücher zum selbstbestimmten Lernen & Persönlichkeitsentwicklung </button></a>
+  </p>
+  <br/> 
+  <br/>  
 </div>
